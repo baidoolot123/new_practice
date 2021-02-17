6efa15876a25312078687aaf16492eeaf8327009
sultan3005 <sultandzusupov@gmail.com>
2021-02-17 19:45:45 +0600

    I add comand and write new code

--- new_file.py
+++ new_file.py
@@ -1,2 +1,9 @@
 print('Message')
 #This is my code 
+#git init
+#git status
+#git checkout -b baidoolot(название ветки)
+#git add (название отправляемого файла)
+#git commit -m "Comment"
+#git push origin baidoolot(название своей ветви)
+print("my code Sultan")
